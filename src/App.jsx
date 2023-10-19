143d67bc581cdf11056421a1b650021ff7855f79
Jordi Bayer <jordi.be.1992@gmail.com>
2023-10-19 20:13:02 +0200

    17.17

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Product from "./pages/Product";
@@ -63,9 +63,9 @@ function App() {
           <Route
             index
             element={
-              <CityList
-                cities={cities}
-                isLoading={isLoading}
+              <Navigate
+                replace
+                to="cities"
               />
             }
           />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,15 +6,11 @@ import { Outlet } from "react-router-dom";
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      Sidebar
       <Logo />
       <AppNav />
       <Outlet />
       <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          {" "}
-          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-        </p>
+        <p className={styles.copyright}> &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.</p>
       </footer>
     </div>
   );
