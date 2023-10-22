8697c4972da37022e8152bb2e063c73c2708ed73
Jordi Bayer <jordi.be.1992@gmail.com>
2023-10-22 19:38:04 +0200

    18.21

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -13,12 +13,11 @@ export default function Homepage() {
           WorldWise keeps track of your adventures.
         </h1>
         <h2>
-          A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+          A world map that tracks your footsteps into every city you can think of. Never forget your wonderful
+          experiences, and show your friends how you have wandered the world.
         </h2>
         <Link
-          to="/app"
+          to="/login"
           className="cta">
           Start tracking now
         </Link>
